c7fea74fcd0698b0713443dd322a1dbabc9b25c3
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-12-02 21:24:17 -0500

    Laid out GUI skeleton with material-ui (closes #15)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import SimpleAppBar from './SimpleAppBar.js'
+import DetailedExpansionPanel from './chooseSensorsPanel.js'
+import DownloadPanel from './downloadPanel.js'
+import LogPanel from './logPanel.js'
 
 class App extends Component {
   render() {
     return (
       <div>
+	<SimpleAppBar />
+	<DetailedExpansionPanel />
+	<DownloadPanel />
+	<LogPanel />
       </div>
     );
   }
---
+++ src/chooseSensorsPanel.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChipsArray from './sensorChips.js';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  icon: {
+    verticalAlign: 'bottom',
+    height: 20,
+    width: 20,
+  },
+  details: {
+    alignItems: 'center',
+  },
+  column: {
+    flexBasis: '33.33%',
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+});
+
+function ChooseSensorsPanel(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <div className={classes.column}>
+            <Typography className={classes.heading}>Sensors</Typography>
+          </div>
+          <div className={classes.column}>
+            <Typography className={classes.secondaryHeading}>Select DASHR units for downloading.</Typography>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.details}>
+          <div className={classes.column} />
+          <div className={classes.column}>
+            <ChipsArray />
+          </div>
+          <div className={classNames(classes.column, classes.helper)}>
+            <Typography variant="caption">
+              Placeholder.
+              <br />
+            </Typography>
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
+
+ChooseSensorsPanel.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ChooseSensorsPanel);
